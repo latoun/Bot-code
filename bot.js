b18cb68c3591e88c26b5e4444ea7894b1c6a04bd
latoun <37518710+latoun@users.noreply.github.com>
2018-03-18 23:53:19 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,9 +8,17 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
-    if (message.content.startsWith(prefix + "ping")) {
-        message.channel.send("pong!");
-  	}
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    switch (command) {
+        case "ping" :
+            message.channel.send('Pong!');
+        break;
+        case "cookies" :
+            let n = args[0]
+            message.channel.send(':cookie' * n);
+        break;
+}
 });
 
 // THIS  MUST  BE  THIS  WAY
