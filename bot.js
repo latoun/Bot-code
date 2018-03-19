977a29e679dccce2ba02383dde7a23d2f57dd599
latoun <37518710+latoun@users.noreply.github.com>
2018-03-19 10:11:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,31 +8,41 @@ client.on('ready', () => {
 
 client.on('message', message => {
     let role = message.guild.roles.find("name", "Chat");
-    if (message.author.bot || !message.content.startsWith(prefix)) return;
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
-    switch (command) {
-        case "ping" :
-            message.channel.send('Pong!');
-        break;
-        case "cookies" :
-            var n = args[0];
-            var s = ''
-            for (var iter = 0; iter < n; iter++) {
-                s += ':cookie:'
+    if (message.author.bot) return;
+    if(!message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/g);
+        for(var iter = 0; iter < args/length; iter++){
+            s = arge[iter].split("")
+            if (s.shift().toLowerCase() == 'd' && s.shift().toLowerCase() == 'i') {
+                message.channel.send(s.join(""));
             }
-            message.channel.send(s);
-        break;
-        case "say" :
-            let text = args.slice(0).join(" ");
-            message.delete();
-            message.channel.send(text);
-        break;
-        case "chat" :
-            let chat = message.mentions.members.first();
         }
-
-
+    }
+    else {
+        const args = message.content.slice(prefix.length).trim().split(/ +/g);
+        const command = args.shift().toLowerCase();
+        switch (command) {
+            case "ping" :
+                message.channel.send('Pong!');
+            break;
+            case "cookies" :
+                var n = args[0];
+                var s = ''
+                for (var iter = 0; iter < n; iter++) {
+                    s += ':cookie:'
+                }
+                message.channel.send(s);
+            break;
+            case "say" :
+                let text = args.slice(0).join(" ");
+                message.delete();
+                message.channel.send(text);
+            break;
+            case "chat" :
+                let chat = message.mentions.members.first();
+            }
+        }    
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
